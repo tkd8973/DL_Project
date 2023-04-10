a428decec3817e94fb95bb9508d13684f426f3b0
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-10 21:38:56 +0900

    dd

--- MH/DL_page.py
+++ MH/DL_page.py
@@ -142,7 +142,7 @@ from PIL import Image
 st.title("딥러닝 모델 구현")
 
 # 모델 불러오기
-model = torch.load("MH/deep/vgg_weights.pth", map_location=torch.device("cpu"))
+model = torch.load("MH/model/vgg_weights.pth", map_location=torch.device("cpu"))
 
 # 이미지 업로드
 uploaded_file = st.file_uploader("이미지 업로드", type=["png", "jpg", "jpeg"])

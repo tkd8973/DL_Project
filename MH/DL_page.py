088220449cbb83b1bc134e14e2b39641bb927307
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-10 21:53:00 +0900

    ㅇㅇ

--- MH/DL_page.py
+++ MH/DL_page.py
@@ -143,6 +143,9 @@ if choice == "페이지1":
         st.title("딥러닝 모델 구현")
         device = torch.device("cpu")  # CPU에서 실행할 경우
         model = torch.load("MH/model/vgg_weights.pth", map_location=device)
+        model_state_dict = model['state_dict']
+        model.load_state_dict(model_state_dict)
+        model.eval()
 
         # 이미지 업로드
         uploaded_file = st.file_uploader("이미지 업로드", type=["png", "jpg", "jpeg"])
@@ -150,10 +153,10 @@ if choice == "페이지1":
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
             st.image(image, caption='업로드한 이미지', use_column_width=True)
-            
+
             # 이미지 전처리
             transform = transforms.Compose([
-                torchvision.transforms.Resize((224, 224)),
+                transforms.Resize((224, 224)),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
@@ -168,6 +171,7 @@ if choice == "페이지1":
             labels = ['class1', 'class2', 'class3'] # 분류 클래스 라벨
             for i in range(len(labels)):
                 st.write(f"{labels[i]}: {probabilities[i]*100:.2f}%")
+
 
 
 
